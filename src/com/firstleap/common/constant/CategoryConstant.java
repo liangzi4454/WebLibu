dd5767a5aafaee8af1f75775d258ecb7b2c6baa5
liangzi4454 <1026163977@qq.com>
2015-02-09 18:01:36 +0800

    update code LHY

---
+++ src/com/firstleap/common/constant/CategoryConstant.java
@@ -0,0 +1,77 @@
+package com.firstleap.common.constant;
+
+public class CategoryConstant {
+	/**
+	 * @author LHY
+	 * 孕妇健康分类
+	 */
+	public static class FirstPregnantType {
+		/** 备孕 **/
+		public static final String PREGNANT_TYPE_1 = "1";
+		/** 备孕常识 **/
+		public static final String PREGNANT_TYPE_101 = "101";
+		/** 孕前检查 **/
+		public static final String PREGNANT_TYPE_102 = "102";
+		/** 营养健康 **/
+		public static final String PREGNANT_TYPE_103 = "103";
+		/** 不孕不育 **/
+		public static final String PREGNANT_TYPE_104 = "104";
+		/** 政策法规 **/
+		public static final String PREGNANT_TYPE_105 = "105";
+		
+		/** 备早 **/
+		public static final String PREGNANT_TYPE_2 = "2";
+		/**  **/
+		public static final String PREGNANT_TYPE_201 = "201";
+		/**  **/
+		public static final String PREGNANT_TYPE_202 = "202";
+		/**  **/
+		public static final String PREGNANT_TYPE_203 = "203";
+		/**  **/
+		public static final String PREGNANT_TYPE_204 = "204";
+		
+		/** 备中 **/
+		public static final String PREGNANT_TYPE_3 = "3";
+		/** 备中 **/
+		public static final String PREGNANT_TYPE_301 = "301";
+		/** 备中 **/
+		public static final String PREGNANT_TYPE_302 = "302";
+		/** 备中 **/
+		public static final String PREGNANT_TYPE_303 = "303";
+		/** 备中 **/
+		public static final String PREGNANT_TYPE_304 = "304";
+		
+		/** 备晚 **/
+		public static final String PREGNANT_TYPE_4 = "4";
+		/**  **/
+		public static final String PREGNANT_TYPE_401 = "401";
+		/**  **/
+		public static final String PREGNANT_TYPE_402 = "402";
+		/**  **/
+		public static final String PREGNANT_TYPE_403 = "403";
+		/**  **/
+		public static final String PREGNANT_TYPE_404 = "404";
+		
+		/** 分娩 **/
+		public static final String PREGNANT_TYPE_5 = "5";
+		/** 分娩方式 **/
+		public static final String PREGNANT_TYPE_501 = "501";
+		/** 注意事项 **/
+		public static final String PREGNANT_TYPE_502 = "502";
+		/**  **/
+		public static final String PREGNANT_TYPE_503 = "503";
+		/**  **/
+		public static final String PREGNANT_TYPE_504 = "504";
+		
+		/** 月子 **/
+		public static final String PREGNANT_TYPE_6 = "6";
+		/** 日常知识 **/
+		public static final String PREGNANT_TYPE_601 = "601";
+		/** 营养健康 **/
+		public static final String PREGNANT_TYPE_602 = "602";
+		/**  **/
+		public static final String PREGNANT_TYPE_603 = "603";
+		/**  **/
+		public static final String PREGNANT_TYPE_604 = "604";
+	}
+}
